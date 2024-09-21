340319e1b03e3a9fb85c5fff855b173d31eef307
hectorLop <lopez.almazan.hector@gmail.com>
2024-09-21 13:34:24 +0200

    feat: Add DnsQuery

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use dns_resolver_rust::header::{Flags, Header, QueryType};
+use std::net::UdpSocket;
+
+use dns_resolver_rust::header::DnsQuery;
 
 fn main() {
-    let header_flags = Flags::default()
-        .query_or_response(false)
-        .kind_of_query(QueryType::Standard)
-        .recursion_desired(true);
-    let _header = Header::new(12345, header_flags, 1, 0, 0, 0);
+    let dns_query = DnsQuery::build_query("www.google.com");
+
+    let dns_server_ip = "8.8.8.8:53";
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    socket.connect(dns_server_ip).unwrap();
+    socket.send(&dns_query.to_bytes()).unwrap();
+
+    let mut buf = [0u8; 1024];
+    let response = socket.recv(&mut buf);
+
+    match response {
+        Ok(response_size) => {
+            println!("Received {response_size} bytes");
+            println!("Content {:?}", &buf[..response_size]);
+        }
+        Err(err) => eprintln!("ERROR {err}"),
+    }
 }
